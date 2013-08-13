a58049ecda3b0e599fe294d6847879cdcd9e130c
jeefuse <yonclv@gmail.com>
2013-08-13 16:08:02 +0800

    首页示例

---
+++ jeefuse-simple/src/main/java/com/jeefuse/index/web/action/IndexAction.java
@@ -0,0 +1,18 @@
+package com.jeefuse.index.web.action;
+
+import com.jeefuse.base.web.action.BaseAction;
+
+/**
+ * 主页.
+ */
+public class IndexAction extends BaseAction {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 首页.
+	 */
+	public String index() {
+		return SUCCESS;
+	}
+
+}
